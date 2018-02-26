88e5349d441b2b4347417123f4354ffff3b99b81
Vinglish <khan.ildar94@gmail.com>
2018-02-26 11:56:12 +0200

    Secret

--- scipt.js
+++ scipt.js
@@ -1,14 +1,19 @@
-var num = +prompt("Мальчик заплатил за бутылку с пробкой 11 рублей. Бутылка стоит на 10 рублей больше, чем пробка. Сколько стоит пробка?(Введите просто число)");
-if(num==50){
-	alert("Правильный ответ")
-}
-else if (num==1) {
-	alert("Нет, совсем нет")
-}
-else if(num<50){
-	alert("Маловато");
-}
-else if(num>50){
-	alert("Это перебор")
-}
 
+/*var word = prompt("input your word"); 
+var code ="";
+for (var i = 0; i < word.length; i++) {
+  var codepoint = word[i].charCodeAt(0); 
+  codepoint+=3;
+  code += String.fromCharCode(codepoint);
+}
+alert(code);
+*/
+var word = prompt("input your word"); 
+var code ="";
+var numback = Number(prompt("Цифра кодировки"));
+for (var i = 0; i < word.length; i++) {
+  var codepoint = word[i].charCodeAt(0); 
+  codepoint-=numback;
+  code += String.fromCharCode(codepoint);
+}
+alert(code);
